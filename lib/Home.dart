ec1215c4a5aba10973c952e93b06ec38070a12b8
Ahmed <A.jamal11300@gmail.com>
2019-07-31 02:56:26 +0300

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -29,14 +29,6 @@ class Home extends StatelessWidget {
                   }));
                 },
             ),
-            new ListTile(
-              title: new Text('Market'),
-              onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Market(null);
-                  }));
-                },
-            ),
           ],
         ),
       ),
@@ -54,14 +46,7 @@ class Home extends StatelessWidget {
                 height: 20,
                 width: 300,
               ),
-              RaisedButton(
-                child: Text('sheel wdy add'),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Add();
-                  }));
-                },
-              ),
+              Text('Our website will let you see a lot of cars type by diffrent prices and quality also you can add a car you want to sellLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptates aspernatur illum? Repellat quae beatae temporibus repudiandae optio perspiciatis doloribus! Veniam quo sit totam maiores, enim ratione non qui repudiandae?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet accusamus ut a vitae. Commodi omnis cumque quas. Ducimus aliquid recusandae architecto unde odit?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, illum architecto consequuntur voluptatibus quae nesciunt? Deserunt tenetur vel maxime, dolore quaerat quos, nemo incidunt inventore corrupti esse, est magnam rerum cupiditate? Cupiditate vel hic blanditiis repudiandae officiis a non eum. Quasi ipsam non labore debitis adipisci. Veritatis porro eaque laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate sit soluta dolorum inventore est? Suscipit dolores molestias corporis atque culpa placeat aspernatur architecto voluptate molestiae fuga est, quos aliquam nobis minus, dicta saepe eius nulla? Assumenda praesentium esse maxime repellat iusto nisi alias recusandae.'),
             ],
           ),
         ),
