05e659a9b492c3b59ebff18b44c373daca0e7cbc
mohammed <moh1999sadan@gmail.com>
2019-07-31 02:15:55 +0300

    Update Add.dart
    designing add screen

--- lib/Add.dart
+++ lib/Add.dart
@@ -20,87 +20,96 @@ class _AddState extends State<Add> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Car'),
+        backgroundColor: Colors.indigo[900],
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: TextField(
-              controller: descriptionController,
-              decoration: InputDecoration(
-                labelText: 'Enter description',
-                fillColor: Colors.white,
-                filled: true,
+      body: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.red[900],
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Enter description',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: TextField(
-              controller: mileageController,
-              decoration: InputDecoration(
-                labelText: 'Enter mileage',
-                fillColor: Colors.white,
-                filled: true,
+            ListTile(
+              title: TextField(
+                controller: mileageController,
+                decoration: InputDecoration(
+                  labelText: 'Enter mileage',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: TextField(
-              controller: aboutController,
-              decoration: InputDecoration(
-                labelText: 'About Car',
-                fillColor: Colors.white,
-                filled: true,
+            ListTile(
+              title: TextField(
+                controller: aboutController,
+                decoration: InputDecoration(
+                  labelText: 'About Car',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: TextField(
-              controller: car_modelController,
-              decoration: InputDecoration(
-                labelText: 'Enter Car Model',
-                fillColor: Colors.white,
-                filled: true,
+            ListTile(
+              title: TextField(
+                controller: car_modelController,
+                decoration: InputDecoration(
+                  labelText: 'Enter Car Model',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: TextField(
-              controller: car_gearController,
-              decoration: InputDecoration(
-                labelText: 'Enter Car Gear',
-                fillColor: Colors.white,
-                filled: true,
+            ListTile(
+              title: TextField(
+                controller: car_gearController,
+                decoration: InputDecoration(
+                  labelText: 'Enter Car Gear',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title:
-            TextField(
-              controller: car_priceController,
-              decoration: InputDecoration(
-                labelText: 'Enter Car Price',
-                fillColor: Colors.white,
-                filled: true,
+            ListTile(
+              title:
+              TextField(
+                controller: car_priceController,
+                decoration: InputDecoration(
+                  labelText: 'Enter Car Price',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-          RaisedButton(
-            child: Text('Add Car'),
-            onPressed: () {
-              var description = descriptionController.text;
-              var mileage = mileageController.text;
-              var about = aboutController.text;
-              var car_model = car_modelController.text;
-              var car_gear = car_gearController.text;
-              var car_price = car_priceController.text;
-              Car car = Car(description, mileage, DateTime.now().toString(),
-                  about, car_model, car_gear, car_price);
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return Market(car);
-              }));
-            },
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Colors.indigo[900],
+              child: Text('Add Car', style: TextStyle(color: Colors.white),),
+              onPressed: () {
+                var description = descriptionController.text;
+                var mileage = mileageController.text;
+                var about = aboutController.text;
+                var car_model = car_modelController.text;
+                var car_gear = car_gearController.text;
+                var car_price = car_priceController.text;
+                Car car = Car(description, mileage, DateTime.now().toString(),
+                    about, car_model, car_gear, car_price);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Market(car);
+                }));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
