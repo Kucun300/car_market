321450c5f1588dfff85a6e19a301102b3717bc7b
Ahmed <A.jamal11300@gmail.com>
2019-07-31 00:04:03 +0300

    Update Model.dart
    comment

--- lib/models/Model.dart
+++ lib/models/Model.dart
@@ -10,4 +10,16 @@ class Car{
   String car_gear;
   String car_price;
   Car(this.description,this.mileage,this.publish_on,this.about,this.car_model,this.car_gear,this.car_price);
+
+  // factory Car.fromJson(Map<String, dynamic>json){
+  //   return Car(
+  //     description: json['description'],
+  //     mileage: json['milage'],
+  //     publish_on: json['publish_on'],
+  //     about: json['about'],
+  //     car_model: json['car_model'],
+  //     car_gear: json['car_gear'],
+  //     car_price: json['car_price'],
+  //   );
+  //}
 }
