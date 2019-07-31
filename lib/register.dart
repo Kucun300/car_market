2cf561020c2bcfa2ab8cf7b95e366411cd7510b7
mohammed <moh1999sadan@gmail.com>
2019-07-31 04:34:45 +0300

    Update register.dart
    designing logout screen

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:car_market/Home.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -8,8 +9,77 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Logout'),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.red[900],
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'mobile number',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password Again',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Colors.indigo[900],
+              child: Text('Logout', style: TextStyle(color: Colors.white),),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Home();
+                })
+                );
+              }
+              )
+          ],
+        ),
+      ),
     );
   }
 }
