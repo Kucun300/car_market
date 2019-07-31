7f1a8987f8f7eb3c39f3440347f2326f0f6cb79e
mohammed <moh1999sadan@gmail.com>
2019-07-31 04:34:28 +0300

    Update login.dart
    designing login screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:car_market/Home.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -8,8 +9,50 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.red[900],
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter Username',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter Password',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Colors.indigo[900],
+              child: Text('Login', style: TextStyle(color: Colors.white),),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Home();
+                })
+                );
+              }
+              )
+          ],
+        ),
+      ),
     );
   }
 }
