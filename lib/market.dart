9d700441371f2ab8e4418b8c7deaa61e08ab3c55
mohammed <moh1999sadan@gmail.com>
2019-07-31 04:10:21 +0300

    adding 2 screens

--- lib/market.dart
+++ lib/market.dart
@@ -17,63 +17,67 @@ class _MarketState extends State<Market> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Car Market'),
-          backgroundColor: Colors.indigo[900],
-        ),
-        body: Container(
-            color: Colors.red[900],
-            child: Center(
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.only(bottom: 10),
-                    margin: EdgeInsets.all(20),
-                    color: Colors.white,
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.account_box,
-                        size: 70,
-                      ),
-                      title: Text(
-                        '${widget.car.description}',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.indigo[900],
-                        ),
-                      ),
-                      subtitle: Text(
-                        '${widget.car.about}',
-                        style: TextStyle(
-                          color: Colors.indigo[900],
-                          fontSize: 16,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return detailes(widget.car);
-                        }));
-                      },
-                    ),
+      appBar: AppBar(
+        title: Text('Car Market'),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Container(
+        color: Colors.red[900],
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(bottom: 10),
+              margin: EdgeInsets.all(20),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(
+                  Icons.account_box,
+                  size: 70,
+                ),
+                title: Text(
+                  '${widget.car.description}',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.indigo[900],
                   ),
-                  SizedBox(
-                    height: 20,
+                ),
+                subtitle: Text(
+                  '${widget.car.about}',
+                  style: TextStyle(
+                    color: Colors.indigo[900],
+                    fontSize: 16,
                   ),
-                  RaisedButton(
-                      color: Colors.indigo[900],
-                      child: Text(
-                        'Home',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return Home();
-                        }));
-                      })
-                ],
+                ),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return detailes(widget.car);
+                  }));
+                },
               ),
-            )));
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Colors.indigo[900],
+              child: Text(
+                'Home',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Home();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
